cbc973ea24db78d337f70264af8829f846f30253
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2020-02-04 00:23:47 +0200

    some

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ var app = express();
 var path = require("path");
 const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get("/", function (req, res) {
-//
-//     res.sendFile(path.join(__dirname+'/public/index.html'));
-//
-// });
 
 app.listen(PORT, function () {
     console.log("port: "+ PORT)
