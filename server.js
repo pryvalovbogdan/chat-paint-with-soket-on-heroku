cb3947974dafdfb649dfc7f39633807ec3cf3ab1
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2020-02-04 00:00:05 +0200

    sss

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 var path = require("path");
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'src')));
-app.get("/", function (req, res) {
-
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-
-});
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get("/", function (req, res) {
+//
+//     res.sendFile(path.join(__dirname+'/public/index.html'));
+//
+// });
 
 app.listen(PORT, function () {
     console.log("port: "+ PORT)
