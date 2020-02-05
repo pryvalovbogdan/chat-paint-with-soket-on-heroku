0af1206e50d09feb2e694addd94b5bbf6f3d53ab
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2020-02-05 16:06:20 +0200

    sss

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const PORT =  process.env.PORT || 5000;
 
 
 const Websocket = require("ws");
-const server = new Websocket.Server({  port: PORT })
+//const server = new Websocket.Server( {port:3000} )
 
-server.on("connection", ws => {
-    ws.send("welcome")
-})
+// server.on("connection", ws => {
+//     ws.send("welcome")
+// })
 
 
 app.use(express.static(path.join(__dirname, 'public')));
