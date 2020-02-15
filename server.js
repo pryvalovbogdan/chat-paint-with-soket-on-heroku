bd975a57636c314d6cb0d8ee91efb4e9c8cb9a63
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2020-02-15 21:00:52 +0200

    ss

--- server.js
+++ server.js
@@ -1,48 +1,33 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-const http = require("http")
-
+let express = require("express");
+let app = express();
+let path = require("path");
 
 const bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({extended: false});
 const PORT =  process.env.PORT || 5000;
-//process.env.PORT ||
-//const servers = http.createServer(app)
-// const Websocket = require("ws");
 
-
-var socket = require("socket.io")
-
-
+let socket = require("socket.io");
+let allArr;
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(PORT, function () {
+let server = app.listen(PORT, function () {
     console.log("port: "+ PORT)
 });
 
 
-var io = socket(server);
+let io = socket(server);
 io.on("connection", function (socket) {
         console.log("socket connection", socket.id)
     socket.on("chat", function (data) {
         io.sockets.emit("chat", data)
-    })
+    });
     socket.on("line", function (data) {
         io.sockets.emit("line", JSON.stringify(data))
 
     })
-})
-// const server = new Websocket.Server( {serverforSoket} )
-//
-// server.on("connection", ws => {
-//     ws.send("welcome")
-// })
-
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var allArr;
 app.post("/saveLine", function (req, res) {
     console.log(req.body.allArr)
     allArr = req.body.allArr;
@@ -53,5 +38,4 @@ app.post("/saveLine", function (req, res) {
 
 app.get("/getLine", function (req, res) {
     res.send(allArr)
-})
-//process.env.PORT ||
+});
