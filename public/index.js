7d98e44d62708fbb9d04bd3b1b558bc5da540c5d
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2020-02-15 20:50:22 +0200

    ss

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
     var socket = io.connect("https://stormy-refuge-28123.herokuapp.com/");
 
     var inputColor = document.querySelector("#color");
